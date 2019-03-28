b5b2908234b08db90c7e2a0d24d1c83100974066
Lili <liliszendrei@gmail.com>
2019-03-28 11:41:11 +0100

    initialize spring project

---
+++ src/main/java/com/hackathon/wowlunteer/WowlunteerApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.wowlunteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowlunteerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WowlunteerApplication.class, args);
+    }
+
+}
